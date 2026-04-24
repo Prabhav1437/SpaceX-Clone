74ab5c35acd448b90698842d506bad869c6eca9c
Prabhav1437 <jainprabhav1437@gmail.com>
2026-04-24 12:00:00 +0530

    feat: add immersive vehicle and spaceflight pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Hero from '../components/Hero'
 import Landing from '../components/Landing'
 
 export default function Home() {
   return (
     <>
+      <Hero />
       <Landing />
     </>
   )
